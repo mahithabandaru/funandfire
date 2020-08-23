f9e81ca060258537bab8ca8fcbfdc112c7a7c038
mahithabandaru <69102117+mahithabandaru@users.noreply.github.com>
2020-08-23 16:14:43 +0530

    Create employee.c

---
+++ employee.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct{
+ 
+    char name[30];
+    int phone;
+    int age;
+    int salary;
+ 
+} Employee;
+ 
+int main()
+{
+    int i, n=20;
+ 
+    Employee employees[n];
+ printf("Enter %d Employee Details \n \n",n);
+    for(i=0; i<n; i++)
+	{
+        printf("Employee %d:- \n",i+1);
+        printf("Name: ");
+        scanf("%s",employees[i].name);
+        printf("Phone: ");
+        scanf("%d",&employees[i].phone);
+        printf("Age :");
+        scanf("%d",&employees[i].age);
+        printf("Salary: ");
+        scanf("%d",&employees[i].salary);
+        printf("\n");
+    } 
+    printf("-------------- All Employees Details ---------------\n");
+ 
+    for(i=0; i<n; i++)
+	{
+ 
+        printf("Name \t: ");
+        printf("%s \n",employees[i].name);
+ 
+        printf("phone \t: ");
+        printf("%d \n",employees[i].phone);
+        
+        printf("Age \t:");
+        printf("%d \n",employees[i].age);
+ 
+        printf("Salary \t: ");
+        printf("%d \n",employees[i].salary);
+ 
+        printf("\n");
+    }
+    
+ 
+    return 0;
+ 
+}
